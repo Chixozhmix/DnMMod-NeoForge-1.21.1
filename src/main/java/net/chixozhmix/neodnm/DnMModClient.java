5f71371c0fbada3b2378c30ed5f81b00635e3690
Chixozhmix <retrojas635@gmail.com>
2026-03-28 12:06:56 +0700

    init

---
+++ src/main/java/net/chixozhmix/neodnm/DnMModClient.java
@@ -0,0 +1,26 @@
+package net.chixozhmix.neodnm;
+
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod(value = MainDnM.MOD_ID, dist = Dist.CLIENT)
+// You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+@EventBusSubscriber(modid = MainDnM.MOD_ID, value = Dist.CLIENT)
+public class DnMModClient {
+    public DnMModClient(ModContainer container) {
+        // Allows NeoForge to create a config screen for this mod's configs.
+        // The config screen is accessed by going to the Mods screen > clicking on your mod > clicking on config.
+        // Do not forget to add translations for your config options to the en_us.json file.
+
+        //container.registerExtensionPoint(IConfigScreenFactory.class, ConfigurationScreen::new);
+    }
+
+    @SubscribeEvent
+    static void onClientSetup(FMLClientSetupEvent event) {
+
+    }
+}
